089d068f3ae2a1854c2eb0df05c96fdc9176cbdb
shubham kumar <shubhofficial1@gmail.com>
2024-12-01 00:22:31 +0530

    Added Lodash dependency & shopping bag using older approach

--- 1.Webpack-basics/index.js
+++ 1.Webpack-basics/index.js
@@ -1,6 +1,11 @@
-var greetingHeading = document.querySelector("#greeting");
-console.log(greetingHeading);
-
 function handleGreetingChange() {
+  const greetingHeading = document.querySelector("#greeting");
   greetingHeading.innerHTML = "Namastey User";
+  const ulElement = document.querySelector(".shopping_bag");
+  const listItems = ["apple", "orange", "banana"];
+  _.forEach(listItems, function (item) {
+    const tempEl = document.createElement("li");
+    tempEl.innerHTML = item;
+    ulElement.appendChild(tempEl);
+  });
 }
